bf8c8dbfc8211d2028ac46d60836b4a91f4e1315
SsilverLeopard <theellaferris@gmail.com>
2025-10-29 09:07:09 +1100

    Initial commit
    set up basic files
    prints "Hello World!" because that's VS's default

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*Create a text adventure game where players navigate a maze of rooms stored in a 2D array.
+* Use a custom class for each type of room, derived from a common Room class. Each type of room should output a custom message or obstacle for the player to overcome. 
+* Your game map can be hand-crafted, or you may choose to use an algorithm to randomly allocate rooms in your 2D maze.
+* Either way, your game does not need to be large (an 4x4 array is sufficient).
+* The focus of this assessment is on your use of inheritance, and your ability to parse and process user input.
+* For this assessment you must create a console application in C++ that includes the following features:
+	* The ability to parse and process commands typed by the user 
+	* For example, 'move east' will move the player to the next room east of their current location. 
+	* Stores a 'maze' of rooms within a 2D array data structure 
+	* Uses custom classes to define a hierarchy of room types. Each room type is to be derived from a common Room class 
+	* Each type of room should output a custom message or obstacle for the player to overcome. 
+* You are free to design all other aspects of the program.
+* You will use version control throughout this assessment to maintain an archive of your work and to track changes. A commit log or screenshot showing the use of version control software is to be submitted with your project.*/
+
+#include <iostream>
+#include "main.h"
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
+
